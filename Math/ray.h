787afc15b33c7cf0cce5a44f6fa182645ee806bd
Егор Дмитриевич Хилик <khilik.egor@niuitmo.ru>
2022-06-05 12:08:09 +0300

    make ray file

---
+++ Math/ray.h
@@ -0,0 +1,31 @@
+//
+// Created by Egor on 22.05.2022.
+//
+
+#ifndef HIRENDER_RAY_H
+#define HIRENDER_RAY_H
+
+
+#include "Vec3.h"
+#include "../Privitives/Point.h"
+
+class ray {
+public:
+    Point orig;
+    Vec3 dir;
+    ray(const Point& origin, const Vec3 direction) : orig(origin), dir(direction){}
+
+    Point origin() const{
+        return orig;
+    }
+    Vec3 direction() const{
+        return dir;
+    }
+
+    Point at(float t) {
+        return orig + dir * t;
+    }
+};
+
+
+#endif //HIRENDER_RAY_H
