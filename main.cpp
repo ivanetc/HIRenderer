e91379648903412c0116b33187e26352ec482978
Егор Дмитриевич Хилик <khilik.egor@niuitmo.ru>
2022-06-22 20:33:59 +0300

    add render RGB square

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#define CHANNEL_NUM 3
 
 int main() {
 
@@ -8,9 +15,8 @@ int main() {
     const int image_height = 600;
 
     // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+    auto* pixels = new uint8_t[image_width * image_height * CHANNEL_NUM];
+    int index = 0;
     for (int j = image_height-1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
@@ -21,7 +27,12 @@ int main() {
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            pixels[index++] = ir;
+            pixels[index++] = ig;
+            pixels[index++] = ib;
+
+           // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    stbi_write_jpg("stbjpg3.jpg", image_width, image_height, 3, pixels, 100);
 }
